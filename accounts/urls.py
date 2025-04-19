9aecd998fd0dc592549c2b8ba0f18dbe96b8768e
KMINGON <rlaalsrhs59@gmail.com>
2025-04-20 02:15:53 +0900

    Feat : 로그인 및 로그아웃 기능 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 # accounts/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('mypage/', views.mypage_view, name='mypage'),
 ]
--- market/settings.py
+++ market/settings.py
@@ -132,3 +132,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
