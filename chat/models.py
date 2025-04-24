3cfc1c9262952bbdbd77c45aab1c0a7d71335515
KMINGON <rlaalsrhs59@gmail.com>
2025-04-24 16:05:36 +0900

    Feat : 1대1 채팅 기능 구현

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Chat(models.Model):
+    sender = models.ForeignKey(CustomUser, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(CustomUser, related_name='received_messages', on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Chat
+from accounts.models import CustomUser
+from django.db.models import Q
+from django.db import models
 
 def global_chat_view(request):
     return render(request, 'chat/global.html')
 
+
+
+@login_required
 def private_chat_view(request, user_id):
-    return render(request, 'chat/private.html', {'user_id': user_id})
+    target_user = get_object_or_404(CustomUser, id=user_id)
+
+    if request.user == target_user:
+        return redirect('/')  # 자기 자신과 채팅 방지
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        if message:
+            Chat.objects.create(
+                sender=request.user,
+                receiver=target_user,
+                message=message
+            )
+            return redirect('private_chat', user_id=target_user.id)
+
+    chats = Chat.objects.filter(
+        (models.Q(sender=request.user, receiver=target_user) |
+         models.Q(sender=target_user, receiver=request.user))
+    )
+
+    return render(request, 'chat/private_chat.html', {
+        'target_user': target_user,
+        'chats': chats
+    })
+
